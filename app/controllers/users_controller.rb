73507ed9f90702887a73e0e20cd1c0371d0082d3
Nolan Morache <nolan241@gmail.com>
2016-11-10 21:47:57 +0000

    Added user partial and design

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   before_action :require_login
   
-  def index
-
-  end
-
+  #fetching all the users and assign them to the @users instance variable
+	def index
+	  @users = User.all
+	end
+	
   def edit
   end
 

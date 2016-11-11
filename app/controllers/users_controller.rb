8e786e5091d4bd8c1a3c702b9165e8551122a9d6
Nolan Morache <nolan241@gmail.com>
2016-11-11 21:44:46 +0000

    Finishing touches on jtinder pluginfor likedislike button

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   before_action :require_login
   
-  #fetching all the users and assign them to the @users instance variable
+  # old fetching all the users and assign them to the @users instance variable.... removed @users = User.all
+  # 
 	def index
-	  @users = User.all
-	end
+	  # getting the users, on the index action, according with the url params sent via the Plugin. 
+	  # using javascript response in index.js.erb under the views/users folder.
+      if params[:id]
+        @users = User.where('id < ?', params[:id]).limit(2)
+      else
+        @users = User.all.limit(2)
+      end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
+	    
+	end	  
 	
   def edit
   end
